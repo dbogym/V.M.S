bfef545a06db533c6ef5a4183a3198398fc29ed2
Go Yeongmin <ko07073@gmail.com>
2025-12-30 00:35:11 +0900

    refactor: @Table 매핑

--- backend/team6/src/main/java/programmers/team6/domain/member/entity/MemberInfo.java
+++ backend/team6/src/main/java/programmers/team6/domain/member/entity/MemberInfo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 import programmers.team6.global.entity.BaseEntity;
 
 @Entity
+@Table(name = "MemberInfo")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberInfo extends BaseEntity {
